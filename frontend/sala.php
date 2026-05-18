2af6ce962b7ae4112a0d611ef387eed31f101c63
BRUNO DE ARAUJO MARTINS <bruno.amartins4@senacsp.edu.br>
2026-05-18 20:58:51 -0300

    fix: alteração no calandario, adicionado ancora nas datas

--- frontend/sala.php
+++ frontend/sala.php
@@ -134,41 +134,41 @@
                         <div>SAB</div>
 
                         <!-- Dias -->
-                        <span>1</span>
-                        <span>2</span>
-                        <span>3</span>
-                        <span>4</span>
-                        <span>5</span>
-                        <span>6</span>
-                        <span>7</span>
-                        <span>8</span>
-                        <span>9</span>
-                        <span>10</span>
-                        <span>11</span>
-                        <span>12</span>
-                        <span>13</span>
-                        <span>14</span>
-                        <span>15</span>
-                        <span>16</span>
-                        <span>17</span>
-                        <span>18</span>
-                        <span>19</span>
-                        <span>20</span>
-                        <span>21</span>
-                        <span>22</span>
-                        <span>23</span>
-                        <span>24</span>
-                        <span>25</span>
-                        <span>26</span>
-                        <span>27</span>
-                        <span>28</span>
-                        <span>29</span>
-                        <span>30</span>
-                        <span>31</span>
-                        <span id="proximo-dia">1</span>
-                        <span id="proximo-dia">2</span>
-                        <span id="proximo-dia">3</span>
-                        <span id="proximo-dia">4</span>
+                        <a href="">1</a>
+                        <a href="">2</a>
+                        <a href="">3</a>
+                        <a href="">4</a>
+                        <a href="">5</a>
+                        <a href="">6</a>
+                        <a href="">7</a>
+                        <a href="">8</a>
+                        <a href="">9</a>
+                        <a herf="">10</a>
+                        <a herf="">11</a>
+                        <a herf="">12</a>
+                        <a herf="">13</a>
+                        <a herf="">14</a>
+                        <a herf="">15</a>
+                        <a herf="">16</a>
+                        <a herf="">17</a>
+                        <a herf="">18</a>
+                        <a herf="">19</a>
+                        <a herf="">20</a>
+                        <a herf="">21</a>
+                        <a herf="">22</a>
+                        <a herf="">23</a>
+                        <a herf="">24</a>
+                        <a herf="">25</a>
+                        <a herf="">26</a>
+                        <a herf="">27</a>
+                        <a herf="">28</a>
+                        <a herf="">29</a>
+                        <a herf="">30</a>
+                        <a herf="">31</a>
+                        <a herf="" id="proximo-dia">1</a>
+                        <a herf="" id="proximo-dia">2</a>
+                        <a herf="" id="proximo-dia">3</a>
+                        <a herf="" id="proximo-dia">4</a>
 
                     </div>
                     <div class="mapa">
@@ -185,7 +185,7 @@
 
                 </div>
                 <div class="avaliacao-box">
-                    <span>Avaliações</span>
+                    <a herf="">Avaliações</a herf="">
 
                     <h1>4.8</h1>
 
