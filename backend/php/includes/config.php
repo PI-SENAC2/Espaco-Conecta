d7cafdf536ab689ab7aa0f05a43e1fd7835e38a3
Augusto <mangacosta4@gmail.com>
2026-06-10 19:55:03 -0300

    FEAT:ligação do banco de dados completo

--- backend/php/includes/config.php
+++ backend/php/includes/config.php
@@ -1,8 +1,13 @@
 <?php 
-$dbHost = 'Localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'espaco_conecta';
+$dbHost = "localhost"; 
+$dbUsername = "root"; 
+$dbPassword = ""; 
+$dbName = "espaco-conecta"; 
 
-$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName); 
+//if ($conexao->connect_errno){
+// echo "erro"} 
+// else{ echo "conexão bem sucedida";
+// } 
 ?>
+
